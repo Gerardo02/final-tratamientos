1ca3f66a96f161cbf9586902958b7c836cc59bd3
gerardo jerez <gerardo.jerez@hotmail.com>
2020-12-01 19:58:04 -0700

    cambios extraños que hay en mi

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
-import sys 
-sys.path.insert(1,'dsp-modulo')
-
 from tkinter import *
-from tkinter.filedialog import askopenfilename
+#import analisis_tiempo_real as atr
+import pyaudio 
+import numpy as np  
+import wave 
 
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-
-from thinkdsp import read_wave
-import numpy
 
 ventana = Tk()
 ventana.title("La ventana")
 ventana.geometry("800x600")
 
+#Formato de audio de microfono
+PROFUNDIDAD_BITS = pyaudio.paInt16
+CANALES = 1
+FRECUENCIA_MUESTREO = 44100
+TIEMPO_GRABACION = 1
+
+#Tamano de Chunk
+CHUNK = 2048
+
+
+
+
 afinacion = StringVar()
 
-def afinar():
+def iniciar():
     afinacion.set("Aprieta la cuerda")
+    if __name__ == "__main__":
+        p = pyaudio.PyAudio()
+        stream = p.open(format = PROFUNDIDAD_BITS, channels = CANALES, rate = FRECUENCIA_MUESTREO, input = True, frames_per_buffer = CHUNK)
 
-btnCalcular = Button(ventana, text = "Inicio", command = afinar)
+        for i in range(0, int(FRECUENCIA_MUESTREO * TIEMPO_GRABACION / CHUNK ))
+
+
+
+
+
+
+
+btnCalcular = Button(ventana, text = "Inicio", command = iniciar)
 btnCalcular.pack()
 
 primeraCuerda = Label(ventana, textvariable = afinacion)
